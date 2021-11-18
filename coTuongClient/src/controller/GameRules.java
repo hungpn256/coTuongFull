cbbb0ff99a262de4b472fe0a36aeaa37a05298a8
Hung Pham <phamnanghung.25@gmail.com>
2021-11-18 10:47:23 +0700

    fix game

--- coTuongClient/src/controller/GameRules.java
+++ coTuongClient/src/controller/GameRules.java
@@ -19,13 +19,12 @@ public class GameRules {
     public GameRules(Piece[][] pieces) {
         this.chessPieces = pieces;
     }
-
-    // wrapper method for whether a piece can move in general
+    
     public boolean canMove(int startX, int startY, int endX, int endY, String name) {
-        int bigX;// the bigger one out of the two Xs
-        int smallX;// the smaller one out of the two Xs
-        int bigY;// the bigger one out of the two Ys
-        int smallY;// the smaller one out of the two Ys
+        int bigX;
+        int smallX;
+        int bigY;
+        int smallY;
         canMove = true;
 
         // assigning values
@@ -60,29 +59,28 @@ public class GameRules {
             this.cannon(bigX, smallX, bigY, smallY, startX, startY, endX, endY);
         } else if (name.equals("兵")) {
             this.pawn1(bigX, smallX, bigY, smallY, startX, startY, endX, endY);
-        } else if (name.equals("兵") || name.equals("卒")) {
+        } else if (name.equals("卒")) {
             this.pawn2(bigX, smallX, bigY, smallY, startX, startY, endX, endY);
         }
         return canMove;
     }
 
-    // individual methods for each piece
     public void rook(int bigX, int smallX, int bigY, int smallY) {
-        if (bigX == smallX) {// if in a vertical line
+        if (bigX == smallX) {
             for (int i = smallY + 1; i < bigY; ++i) {
                 if (chessPieces[bigX][i] != null) {
                     canMove = false;
                     break;
                 }
             }
-        } else if (bigY == smallY) {// in a horizontal line
+        } else if (bigY == smallY) {
             for (int i = smallX + 1; i < bigX; ++i) {
                 if (chessPieces[i][bigY] != null) {
                     canMove = false;
                     break;
                 }
             }
-        } else {// not in a straight line
+        } else {
             canMove = false;
         }
     }
@@ -91,8 +89,7 @@ public class GameRules {
         int a = bigX - smallX;
         int b = bigY - smallY;
 
-        if ((a == 1) && (b == 2)) {// vertical rectangle
-            // game rules
+        if ((a == 1) && (b == 2)) {
             if (startY > endY) {
                 if (chessPieces[startX][startY - 1] != null) {
                     canMove = false;
@@ -102,8 +99,7 @@ public class GameRules {
                     canMove = false;
                 }
             }
-        } else if ((a == 2) && (b == 1)) {// horizontal rectangle
-            // game rules
+        } else if ((a == 2) && (b == 1)) {
             if (startX > endX) {
                 if (chessPieces[startX - 1][startY] != null) {
                     canMove = false;
@@ -123,8 +119,8 @@ public class GameRules {
         int a = bigX - smallX;
         int b = bigY - smallY;
 
-        if ((a == 2) && (b == 2)) {// 2*2 square
-            if (endY > 4) {// bishop cannot go across the river
+        if ((a == 2) && (b == 2)) {
+            if (endY > 4) {
                 canMove = false;
             }
             if (chessPieces[(bigX + smallX) / 2][(bigY + smallY) / 2] != null) {// game rule
@@ -140,8 +136,8 @@ public class GameRules {
         int a = bigX - smallX;
         int b = bigY - smallY;
 
-        if ((a == 2) && (b == 2)) {// 2*2 square
-            if (endY < 5) {// bishop cannot go across the river
+        if ((a == 2) && (b == 2)) {
+            if (endY < 5) {
                 canMove = false;
             }
             if (chessPieces[(bigX + smallX) / 2][(bigY + smallY) / 2] != null) {// game rule
@@ -157,17 +153,17 @@ public class GameRules {
         int a = bigX - smallX;
         int b = bigY - smallY;
 
-        if ((a == 1) && (b == 1)) {// need to be 1 unit diagonal
-            if (startY > 4) {// guard below
-                if (endY < 7) {// can't exit the palace vertically
+        if ((a == 1) && (b == 1)) {
+            if (startY > 4) {
+                if (endY < 7) {
                     canMove = false;
                 }
-            } else {// guard above
-                if (endY > 2) {// can't exit the palace vertically
+            } else {
+                if (endY > 2) {
                     canMove = false;
                 }
             }
-            if (endX > 5 || endX < 3) {// can't exit the palace horizontally
+            if (endX > 5 || endX < 3) {
                 canMove = false;
             }
         } else {
@@ -180,17 +176,17 @@ public class GameRules {
         int a = bigX - smallX;
         int b = bigY - smallY;
 
-        if (((a == 1) && (b == 0)) || ((a == 0) && (b == 1))) {// 1 non-diagonal unit only
-            if (startY > 4) {// king below
-                if (endY < 7) {// can't exit the palace vertically
+        if (((a == 1) && (b == 0)) || ((a == 0) && (b == 1))) {
+            if (startY > 4) {
+                if (endY < 7) {
                     canMove = false;
                 }
-            } else {// king above
-                if (endY > 2) {// can't exit the palace vertically
+            } else {
+                if (endY > 2) {
                     canMove = false;
                 }
             }
-            if (endX > 5 || endX < 3) {// can't exit the palace horizontally
+            if (endX > 5 || endX < 3) {
                 canMove = false;
             }
         } else {
@@ -199,28 +195,28 @@ public class GameRules {
     }
 
     public void cannon(int bigX, int smallX, int bigY, int smallY, int startX, int startY, int endX, int endY) {
-        if (bigX == smallX) {// vertical line
-            if (chessPieces[endX][endY] != null) {// hitting opponent's pieces
+        if (bigX == smallX) {
+            if (chessPieces[endX][endY] != null) {
                 int count = 0;
                 for (int i = smallY + 1; i < bigY; ++i) {
                     if (chessPieces[smallX][i] != null) {
                         ++count;
                     }
                 }
-                if (count != 1) {// must have exactly one piece in between
+                if (count != 1) {
                     canMove = false;
                 }
-            } else if (chessPieces[endX][endY] == null) {// not hitting opponent's pieces
+            } else if (chessPieces[endX][endY] == null) {
 
                 for (int i = smallY + 1; i < bigY; ++i) {
-                    if (chessPieces[smallX][i] != null) {// cannot allow pieces in middle
+                    if (chessPieces[smallX][i] != null) {
                         canMove = false;
                         break;
                     }
                 }
             }
-        } else if (bigY == smallY) {// horizontal line
-            if (chessPieces[endX][endY] != null) {// hitting opponent's pieces
+        } else if (bigY == smallY) {
+            if (chessPieces[endX][endY] != null) {
                 int count = 0;
 
                 for (int i = smallX + 1; i < bigX; ++i) {
@@ -228,13 +224,13 @@ public class GameRules {
                         ++count;
                     }
                 }
-                if (count != 1) {// must have exactly one piece in between
+                if (count != 1) {
                     canMove = false;
                 }
-            } else if (chessPieces[endX][endY] == null) {// not hitting opponent's pieces
+            } else if (chessPieces[endX][endY] == null) {
 
                 for (int i = smallX + 1; i < bigX; ++i) {
-                    if (chessPieces[i][smallY] != null) {// cannot allow any pieces in between
+                    if (chessPieces[i][smallY] != null) {
                         canMove = false;
                         break;
                     }
@@ -246,19 +242,19 @@ public class GameRules {
     }
 
     public void pawn1(int bigX, int smallX, int bigY, int smallY, int startX, int startY, int endX, int endY) {
-        if (startY < 5) {// if not across the river yet
-            if (startX != endX) {// cannot move horizontally
+        if (startY < 5) {
+            if (startX != endX) {
                 canMove = false;
             }
             if (endY - startY != 1) {
                 canMove = false;
             }
-        } else {// crossed the river
-            if (startX == endX) {// vertical line
+        } else {
+            if (startX == endX) {
                 if (endY - startY != 1) {
                     canMove = false;
                 }
-            } else if (startY == endY) {// horizontal line
+            } else if (startY == endY) {
                 if (bigX - smallX != 1) {
                     canMove = false;
                 }
@@ -269,19 +265,19 @@ public class GameRules {
     }
 
     public void pawn2(int bigX, int smallX, int bigY, int smallY, int startX, int startY, int endX, int endY) {
-        if (startY > 4) {// if not across the river yet
-            if (startX != endX) {// cannot move horizontally
+        if (startY > 4) {
+            if (startX != endX) {
                 canMove = false;
             }
             if (endY - startY != -1) {
                 canMove = false;
             }
-        } else {// crossed the river
-            if (startX == endX) {// vertical line
+        } else {
+            if (startX == endX) {
                 if (endY - startY != -1) {
                     canMove = false;
                 }
-            } else if (startY == endY) {// horizontal line
+            } else if (startY == endY) {
                 if (bigX - smallX != 1) {
                     canMove = false;
                 }
